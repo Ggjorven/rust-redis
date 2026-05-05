949cd61828d0b58853dd6b82cc00e20de5daa4da
ggjorven <jorbenvanderwal@gmail.com>
2026-05-05 21:34:09 +0200

    Actually handle errors.

--- redis/src/main.rs
+++ redis/src/main.rs
@@ -1,12 +1,17 @@
+use std::process::ExitCode;
+
 mod resp;
 mod redis;
 mod server;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> ExitCode {
     let mut server = server::Server::new().await;
-    server.listen(Some(6379)).await;
-    server.run_loop().await;
+    
+    let Ok(_) = server.listen(Some(6379)).await else { return ExitCode::FAILURE; };
+    let Ok(_) = server.run_loop().await else { return ExitCode::FAILURE; };
+
+    ExitCode::SUCCESS
 }
 
 // For tests
